e901c0288d177836046c8205882d7bbdf2a28b13
EnkiW <ytcahekyttpocto@gmail.com>
2023-03-03 23:50:37 +0200

    2 button

--- jQuerySelect.js
+++ jQuerySelect.js
@@ -14,4 +14,24 @@ $(document).ready(function() {
             top: 0
         });
     });
+    $("#moveButton2").click(function () {
+        var box = $('#object');
+        box.animate({
+            left: 175
+        });
+        box.animate({
+            left: 350,
+            top: 350
+        });
+        box.animate({
+            left: 0
+        });
+        box.animate({
+            left:175,
+            top: 0
+        });
+        box.animate({
+            left:0
+        });
+    });
 });
